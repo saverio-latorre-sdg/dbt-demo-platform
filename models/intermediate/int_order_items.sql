d7bd2276017426e9e6300e7457300515b0ab19d5
saverio-latorre-sdg <138021864+saverio-latorre-sdg@users.noreply.github.com>
2026-04-02 15:07:20 +0200

    feat: testing column cicd
    - added return_flag int_order_items 
    - removed return_flag  from fct_orders
    - added test return_fl
    - added pk intermediate layer

--- models/intermediate/int_order_items.sql
+++ models/intermediate/int_order_items.sql
@@ -1,50 +1,53 @@
-with lineitems as (
+with
+    lineitems as (select * from {{ ref("stg_line_items") }}),
 
-    select * from {{ ref('stg_line_items') }}
+    orders as (select * from {{ ref("stg_orders") }}),
 
-),
+    final as (
 
-orders as (
+        select
+            -- surrogate key
+            {{ dbt_utils.generate_surrogate_key(["li.order_key", "li.line_number"]) }}
+            as order_item_key,
 
-    select * from {{ ref('stg_orders') }}
+            -- keys
+            li.order_key,
+            li.line_number,
+            li.part_key,
+            li.supplier_key,
+            o.customer_key,
 
-),
+            -- dates
+            o.order_date,
+            li.ship_date,
+            li.commit_date,
+            li.receipt_date,
 
-final as (
+            -- degenerate dimensions
+            o.status_code as order_status_code,
+            o.priority_code as order_priority_code,
+            case
+                when li.return_flag = 'R'
+                then 'returned'
+                when li.return_flag = 'A'
+                then 'accepted'
+                else 'na'
+            end as return_flag,
+            li.line_status,
+            li.ship_mode,
 
-    select
-        -- keys
-        li.order_key,
-        li.line_number,
-        li.part_key,
-        li.supplier_key,
-        o.customer_key,
+            -- measures
+            li.quantity,
+            li.extended_price,
+            li.discount_percentage,
+            li.tax_rate,
+            li.net_item_sales_amount,
+            li.gross_item_sales_amount
 
-        -- dates
-        o.order_date,
-        li.ship_date,
-        li.commit_date,
-        li.receipt_date,
+        from lineitems li
+        inner join orders o on li.order_key = o.order_key
 
-        -- degenerate dimensions
-        o.status_code           as order_status_code,
-        o.priority_code         as order_priority_code,
-        li.return_flag,
-        li.line_status,
-        li.ship_mode,
+    )
 
-        -- measures
-        li.quantity,
-        li.extended_price,
-        li.discount_percentage,
-        li.tax_rate,
-        li.net_item_sales_amount,
-        li.gross_item_sales_amount
-
-    from lineitems li
-    inner join orders o
-        on li.order_key = o.order_key
-
-)
-
-select * from final
+select *
+from final
--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,49 +1,48 @@
-with order_items as (
+with
+    order_items as (select * from {{ ref("int_order_items") }}),
 
-    select * from {{ ref('int_order_items') }}
+    final as (
 
-),
+        select
+            -- surrogate key
+            {{ dbt_utils.generate_surrogate_key(["order_key", "line_number"]) }}
+            as order_item_key,
 
-final as (
+            -- foreign keys
+            order_key,
+            line_number,
+            customer_key,
+            supplier_key,
+            part_key,
 
-    select
-        -- surrogate key
-        {{ dbt_utils.generate_surrogate_key(['order_key', 'line_number']) }} as order_item_key,
+            -- dates
+            order_date,
+            ship_date,
+            commit_date,
+            receipt_date,
 
-        -- foreign keys
-        order_key,
-        line_number,
-        customer_key,
-        supplier_key,
-        part_key,
+            -- degenerate dimensions (attributes of the event, no dedicated dim)
+            order_status_code,
+            order_priority_code,
+            -- return_flag,
+            line_status,
+            ship_mode,
 
-        -- dates
-        order_date,
-        ship_date,
-        commit_date,
-        receipt_date,
+            -- measures
+            quantity,
+            extended_price,
+            discount_percentage,
+            tax_rate,
+            net_item_sales_amount,
+            gross_item_sales_amount,
 
-        -- degenerate dimensions (attributes of the event, no dedicated dim)
-        order_status_code,
-        order_priority_code,
-        return_flag,
-        line_status,
-        ship_mode,
+            -- derived measures
+            datediff('day', order_date, ship_date) as days_to_ship,
+            datediff('day', ship_date, receipt_date) as days_in_transit
 
-        -- measures
-        quantity,
-        extended_price,
-        discount_percentage,
-        tax_rate,
-        net_item_sales_amount,
-        gross_item_sales_amount,
+        from order_items
 
-        -- derived measures
-        datediff('day', order_date, ship_date)      as days_to_ship,
-        datediff('day', ship_date, receipt_date)    as days_in_transit
+    )
 
-    from order_items
-
-)
-
-select * from final
+select *
+from final
