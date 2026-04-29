6aa42b0eec89b6b6960928abf0c4ed927b304fd5
saverio-latorre-sdg <138021864+saverio-latorre-sdg@users.noreply.github.com>
2026-04-29 12:31:54 +0200

    Visual editor/7531/200536 (#5)
    * feat: added agg_regional_sales

---
+++ models/agg_regional_sales.sql
@@ -0,0 +1,59 @@
+WITH fct_orders AS (
+  SELECT
+    *
+  FROM {{ ref('fct_orders') }}
+), dim_customers AS (
+  SELECT
+    *
+  FROM {{ ref('dim_customers') }}
+), join_1 AS (
+  SELECT
+    *
+  FROM fct_orders
+  LEFT JOIN dim_customers
+    USING (CUSTOMER_KEY)
+), aggregate_1 AS (
+  SELECT
+    CUSTOMER_REGION,
+    SUM(NET_ITEM_SALES_AMOUNT) AS sum_NET_ITEM_SALES_AMOUNT
+  FROM join_1
+  GROUP BY
+    CUSTOMER_REGION
+), rename_1 AS (
+  SELECT
+    CUSTOMER_REGION AS CUSTOMER_REGION,
+    sum_NET_ITEM_SALES_AMOUNT AS NET_SALES_AMOUNT,
+    *
+    EXCLUDE (CUSTOMER_REGION, sum_NET_ITEM_SALES_AMOUNT)
+  FROM aggregate_1
+), order_1 AS (
+  SELECT
+    *
+  FROM rename_1
+  ORDER BY
+    NET_SALES_AMOUNT DESC
+), formula_1 AS (
+  SELECT
+    *,
+    RANK() OVER (ORDER BY NET_SALES_AMOUNT DESC) AS rn
+  FROM order_1
+), filter_1 AS (
+  SELECT
+    *
+  FROM formula_1
+  WHERE
+    rn <= '3'
+), "order" AS (
+  SELECT
+    *
+  FROM filter_1
+  ORDER BY
+    rn DESC
+), agg_regional_sales_sql AS (
+  SELECT
+    *
+  FROM "order"
+)
+SELECT
+  *
+FROM agg_regional_sales_sql
